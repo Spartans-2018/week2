ca4859ceaddec311ec8c9d1a0b52ae1ebd00beb4
boyanb9184 <boyan.benev@gmail.com>
2018-12-06 19:44:33 -0500

    changes to some files

---
+++ 03_oops_classes_scope/exercises/mcnuggets/mcnuggets-class-1.py
@@ -0,0 +1,22 @@
+class McNuggets:
+
+    def __init__(self, n):
+        self.n=n
+        self.res= 'This is not a Mcnuggets number'
+
+
+    def calc(self):
+
+        for i in range (0, self.n//6+1):
+            for j in range (0, self.n//9+1):
+                for k in range (0, self.n//20+1):
+                    #print ("trying for %2d: %2d %2d %2d" % (n,i,j,k))
+                    if 6*i+9*j+20*k==self.n:
+                        self.res= (i, j, k)
+        # if self.res==():
+        #     print( 'this is not a mcnuggets number')
+        #
+
+a=McNuggets(80)
+a.calc()
+print (a.res)
---
+++ 03_oops_classes_scope/exercises/mcnuggets/mcnuggets.py
@@ -0,0 +1,12 @@
+def mcnugg(n):
+    for i in range (0, n//6+1):
+        for j in range (0, n//9+1):
+            for k in range (0, n//20+1):
+                print ("trying for %2d: %2d %2d %2d" % (n,i,j,k))
+                if 6*i+9*j+20*k==n:
+                    return (i,j,k)
+
+
+
+
+print (mcnugg(80))
