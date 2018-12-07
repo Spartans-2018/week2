9df10c9ddbee5749879c9793aa7f45d5e3ba5bd5
boyanb9184 <boyan.benev@gmail.com>
2018-12-06 20:03:09 -0500

    some chages

--- 03_oops_classes_scope/exercises/mcnuggets/mcnuggets.py
+++ 03_oops_classes_scope/exercises/mcnuggets/mcnuggets.py
@@ -2,7 +2,7 @@ def mcnugg(n):
     for i in range (0, n//6+1):
         for j in range (0, n//9+1):
             for k in range (0, n//20+1):
-                print ("trying for %2d: %2d %2d %2d" % (n,i,j,k))
+                #print ("trying for %2d: %2d %2d %2d" % (n,i,j,k))
                 if 6*i+9*j+20*k==n:
                     return (i,j,k)
 
@@ -10,3 +10,5 @@ def mcnugg(n):
 
 
 print (mcnugg(80))
+
+print (mcnugg(18))
